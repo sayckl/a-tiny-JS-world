90a44f112c0df93b846614cb1c400ad0ae786fa5
sayckl <34318594+sayckl@users.noreply.github.com>
2019-02-04 15:40:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+const dog = {
+  species: 'dog',
+  name: 'Dobby',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'woof-woof!',
+  friend: 'Melinda'
+};
+const cat = {
+  species: 'cat',
+  name: 'Gary',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'meo!',
+  friend: 'Tommy'
+};
+const man = {
+  species: 'human',
+  name: 'Tommy',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  saying: 'I WANT TO SEE BLOOD OF MY ENEMIES!',
+  friend: 'Gary'
+};
+const woman = {
+  species: 'human',
+  name: 'Melinda',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: 'Gender equality!',
+  friend: 'Dobby'
+};
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+function printing (obj) {
+  print([obj.species, obj.name, obj.gender, obj.legs, obj.hands, obj.saying, obj.friend].join("; "));
+}
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+[dog, car, man, woman].forEach(printing);
